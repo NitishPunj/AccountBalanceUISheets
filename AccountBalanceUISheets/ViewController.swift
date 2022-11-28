78bc61f04ec2063904bcf2e13818b6ecbdcee139
Nitish Sharma <nitish.sharma@lloydsbanking.com>
2022-11-28 12:30:20 +0000

    Initial Commit

---
+++ AccountBalanceUISheets/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AccountBalanceUISheets
+//
+//  Created by Sharma, Nitish (GT RET Consumer Serv EL- Delivery L, Group Transformation) on 28/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AccountBalanceUISheetsUITests/AccountBalanceUISheetsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  AccountBalanceUISheetsUITestsLaunchTests.swift
+//  AccountBalanceUISheetsUITests
+//
+//  Created by Sharma, Nitish (GT RET Consumer Serv EL- Delivery L, Group Transformation) on 28/11/2022.
+//
+
+import XCTest
+
+class AccountBalanceUISheetsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
